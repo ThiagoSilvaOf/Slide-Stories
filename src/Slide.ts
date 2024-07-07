6de7fb2f92c79d65e44f39563239a139c28912af
Thiago Silva <titusilvs91@gmail.com>
2024-07-07 00:21:40 -0300

    Show Slide

--- src/Slide.ts
+++ src/Slide.ts
@@ -1,23 +1,36 @@
 export default class Slide {
   public container;
-  public elements;
+  public slides;
   public controls;
   public time;
+  public index;
+  public slide;
 
   constructor(
     container: Element,
-    elements: Element[],
+    slides: Element[],
     controls: Element,
-    time: number = 5000
+    time: number = 5000,
   ) {
     this.container = container;
-    this.elements = elements;
+    this.slides = slides;
     this.controls = controls;
-    this.time = time;
+    this.time = time; 
+  
+    this.index = 0;
+    this.slide = this.slides[this.index]
 
-    console.log(this.container);
-    console.log(this.elements);
-    console.log(this.controls);
-    console.log(this.time);    
+    this.show(this.index)
+  }
+
+  hide(el:Element){
+    el.classList.remove("active");
+  }
+
+  show(index:number){
+    this.index = index;
+    this.slide = this.slides[this.index]
+    this.slides.forEach(slide => this.hide(slide));
+    this.slide.classList.add("active")
   }
 }
